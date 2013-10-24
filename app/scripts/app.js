3e935e61be972044cebc255131fe17209b709273
JM Huret <jm.huret@pointsourcellc.com>
2013-10-24 15:56:13 -0400

    Created initial pass at PS Icon

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('psiconApp', [])
+  .config(function ($routeProvider, $compileProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+      //Allows for links to exist with the follow protocols prefixes
+      $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|filesystem|mailto|chrome-extension):/);
+  });
